f8fa7bec3128b8b253acf5813536ae6c048d0862
bobby.bash_ <72820018+bobby-bash@users.noreply.github.com>
2023-11-20 10:49:06 +0530

    Create echoclient.py

---
+++ echoclient.py
@@ -0,0 +1,12 @@
+import socket
+import sys
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address=('localhost',10000)
+client.connect(server_address)
+message=input()
+print("sending ",message)
+client.sendall(message.encode())
+print("original:",message)
+data=client.recv(1000).decode()
+print("ECHO:",data)
+client.close()
